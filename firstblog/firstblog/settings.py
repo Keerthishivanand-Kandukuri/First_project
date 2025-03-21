7559008a9d22edac2a1ce31b391032200d613386
Keerthishivanand-Kandukuri <Kandukurikeerthishivanand@gmail.com>
2025-03-21 23:18:42 +0530

    Update settings.py
    update setting

--- firstblog/firstblog/settings.py
+++ firstblog/firstblog/settings.py
@@ -16,8 +16,8 @@ from os import path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-TEMPLATES_DIR = path.join(BASE_DIR, 'templates')
+DIR_temp = path.join(BASE_DIR, 'firstblog')
+TEMPLATES_DIR = path.join(DIR_temp, 'firstblog')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
