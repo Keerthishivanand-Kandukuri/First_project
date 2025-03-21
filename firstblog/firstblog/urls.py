ae094b11ce218c24f1fc72581f75979c52205584
Keerthishivanand-Kandukuri <Kandukurikeerthishivanand@gmail.com>
2025-03-21 23:39:49 +0530

    Update urls.py

--- firstblog/firstblog/urls.py
+++ firstblog/firstblog/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
     path('home/', views.home ,name='home'),
     path('create_post/', views.create_post, name='create_post'),
     path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
